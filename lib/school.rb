a97bde74406e92cb171d0ac081537bc62ac4a134
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-30 14:10:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -17,9 +17,9 @@ class School
     
     @roster [student_grade] =[]
     @roster[student_grade] << student_name
-    
-  end 
-  binding.pry
+     binding.pry
+  end
+  
 end
 
 #school = School.new #("Bayside High School")
