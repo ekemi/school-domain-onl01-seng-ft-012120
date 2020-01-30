8f2b54e9f14df06d3c667e303465569eba84aa02
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-30 13:49:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,8 +15,8 @@ class School
   
   def add_student (student_name, student_grade)
     
-    @roster [student_name]=[]
-    @roster[student_name] << student_grade
+    @roster [student_grade]=[]
+    @roster[student_grade] << student_name
     
   end 
 end
